c62901a02d5e70426bf622ada3da917ea8d27ed7
«vitaly» <vit21513@yandex.ru>
2022-07-06 21:07:11 +0300

    temp7

---
+++ ex007/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+int x = xa, y =xb;
+int count = 0;
+
+while(count < 10)
+{
+    int what = new Random().Next(0,3)
+}
