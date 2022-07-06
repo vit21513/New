430dd226887ec66bad053fa29bdc753b95c5abd8
«vitaly» <vit21513@yandex.ru>
2022-07-06 20:30:18 +0300

    if add

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите   ");
+string username = Console.ReadLine();
+
+if(username == "Марфа")
+{
+    Console.WriteLine("это Марфа");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);   
+}
--- examples3/Program.cs
+++ examples3/Program.cs
@@ -4,3 +4,4 @@ int numberB = new Random().Next(1,10);
 Console.WriteLine(numberB);
 int result = numberA+numberB;
 Console.WriteLine(result);
+
