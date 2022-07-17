885872b7be853d905455d89908ec26a3553c16db
«vitaly» <vit21513@yandex.ru>
2022-07-17 14:03:41 +0300

    f

--- leccia170722/Program.cs
+++ leccia170722/Program.cs
@@ -110,23 +110,24 @@ void PrintArray(int[] array)
   Console.WriteLine(); 
 }
 PrintArray(arr);
+
+
 void SelectionSort(int[] array)
 {
   for (int i = 0; i<array.Length; i++)
-  {int minPosition = i;
-  for (int j=i+1;j<array.Length;j++)
   {
-    if (array[j] < array[minPosition]) minPosition =j;
-    int temporary =array[i];
+    int minPosition = i;
+    for (int j=i+1;j<array.Length;j++)
+    {
+      if (array[j] < array[minPosition]) minPosition =j;
+      }
+  int temporary =array[i];
   array[i] = array[minPosition];
   array[minPosition] = temporary;
-    
-  }
-
-
   
   }
 
 }
 
 SelectionSort(arr);
+PrintArray(arr);
