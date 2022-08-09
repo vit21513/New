6fd9c51bb38ab5c862dec198a9a7a77cbb9970f6
«vitaly» <vit21513@yandex.ru>
2022-08-09 18:17:50 +0300

    09082022

--- examples3/Program.cs
+++ examples3/Program.cs
@@ -1,7 +1,53 @@
-﻿int numberA = new Random().Next(1,10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10);
-Console.WriteLine(numberB);
-int result = numberA+numberB;
-Console.WriteLine(result);
+﻿
 
+double[] CreateArrayRndDouble(int size, int len)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(rnd.NextDouble() * len, 1);
+    }
+    return arr;
+}
+
+double DifferenMaxMin(double[] array)
+{
+    double maxAr = array[0];
+    double minAr = array[0];
+    double deff = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > maxAr)
+        {
+            maxAr = array[i];
+        }
+        if (array[i] < minAr)
+        {
+            minAr = array[i];
+        }
+
+    }
+     
+    deff = maxAr - minAr;
+    return deff;
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + "| ");
+        else Console.Write(array[i] + "]");
+    }
+}
+
+double[] array = CreateArrayRndDouble(5, 10);
+PrintArray(array);
+Console.WriteLine();
+double result = Math.Round(DifferenMaxMin(array), 1);    // тоже округлил, так иногда выдает кучу знаков после запятой
+Console.WriteLine($" разница между максимальным иминимальным элементов массива  составляет {result} ");
---
+++ leccia170722/2chast/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+//double Factorial(double n)
+//{
+//    if (n ==1) return 1;
+//    else return n*Factorial(n-1);
+//}
+
+//for (int i = 1; i < 40; i++)
+//{
+//    Console.WriteLine($" {i}! {Factorial(i)}");
+//}
+
+//Console.WriteLine(Factorial(5));
+
+// f(1) =1
+// f(2) =2
+// f(n) = f(n-1) + f(n-2)
+
+double Fibanacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibanacci(n-1) + Fibanacci(n-2);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibanacci(i));    
+}
