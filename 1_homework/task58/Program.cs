994b13679734366ecc588b533b9267e664b1fc52
«vitaly» <vit21513@yandex.ru>
2022-08-16 12:58:27 +0300

    try home

---
+++ 1_homework/task58/Program.cs
@@ -0,0 +1,89 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+//Например, заданы 2 массива:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+
+//1 5 8 5
+//4 9 4 2
+//7 2 2 6
+// 3 4 7
+//Их произведение будет равно следующему массиву:
+//1 20 56 10
+//20 81 8 6
+//56 8 4 24
+//10 6 24 49
+
+//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3},");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int UserInputInt(string userInputStr)
+{
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+
+}
+
+int[,] MultiplicationMatrix(int[,] array1,int[,] array2,int line,int stolb)
+{
+    int [,] multiMatrix  = new int [line,stolb];
+    for (int i = 0; i < multiMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < multiMatrix.GetLength(1); j++)
+        {
+           multiMatrix [i,j] = array1[i,j] * array2[i,j];
+        }
+       
+    } 
+    return multiMatrix;
+}
+Console.Clear();
+Console.Clear();
+int m = UserInputInt("Введите   длину строки масива ");
+int n = UserInputInt("Введите  длину  столбцов масива ");
+
+int[,] firstmatrix = GetMatrix(m,n);
+int[,] secondmatrix = GetMatrix(m,n);
+PrintMatrix(firstmatrix);
+Console.WriteLine();
+PrintMatrix(secondmatrix);
+
+Console.WriteLine();
+int[,] resultmatrix = MultiplicationMatrix(firstmatrix,secondmatrix,m,n);
+PrintMatrix(resultmatrix);
--- 1zad/Program.cs
+++ 1zad/Program.cs
@@ -1,67 +1,60 @@
-﻿//0. Демонстрация решения
-//Напишите программу, которая на вход
-//принимает число и выдаёт его квадрат (число
-//умноженное на само себя).
-//Например:
-//4 -> 16
-//-3 -> 9
-//-7 -> 49
-//Console.Write("Введите целое число");
-//int num = Convert.ToInt32(Console.ReadLine());
-//int num = 4; // -2.147 до 2.147 млрд
-//int res= num * num;
-//Console.WriteLine($"Квадрат чмсла {num} ={res}");
+﻿//Задача 50. Напишите программу, которая на вход принимает 
+//позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//1, 7 -> такого числа в массиве нет
 
-//Console.Write("Введите первое число ");
-//int first = Convert.ToInt32(Console.ReadLine());
-//Console.Write("Введите Второе число ");
-//int second = Convert.ToInt32(Console.ReadLine());
-//if (first == second*second)
-//{
-//Console.WriteLine("Да");
-//}
-//else
-//Console.WriteLine("Нет");
 
-Console.Write("введите день недели ");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day == 1)
+int[,] GetMatrix(int m, int n)
 {
-    Console.WriteLine("Понедельник");
-  
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 100);
+        }
+    }
+    return matrix;
 }
-else if (day == 2)
+
+void PrintMatrix(int[,] array)
 {
-    Console.WriteLine("Вторник");
-  
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < array.GetLength(1) - 1) Console.Write($"{array[i, j],3},");
+            else Console.Write($"{array[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
 }
-else if (day == 3)
+
+int UserInputInt(string userInputStr)
 {
-    Console.WriteLine("Среда");
-  
+    Console.WriteLine(userInputStr);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+
 }
-else if (day == 4)
+
+void PrintIndexMatrix(int[,] array, int indStrok, int indexStolb, int indStr, int indS)
 {
-    Console.WriteLine("Четверг");
-  
+    if (indStrok > indStr || indexStolb > indS) Console.WriteLine("такого элемента нет в массиве");
+    else if (indStrok <= 0 || indexStolb <= 0) Console.WriteLine("такого элемента нет в массиве");
+    else Console.WriteLine($" Значение числа по введенному индексу составляет {array[indStrok - 1, indexStolb - 1]}");
 }
-else if (day == 5)
-{
-    Console.WriteLine("Пятница");
-  
-}
-else if (day == 6)
-{
-    Console.WriteLine("Субота");
-  
-}
-else if (day == 7)
-{
-    Console.WriteLine("Воскресенье");
-  
-}
-else if (day > 7)
-{
-    Console.WriteLine("нет больше дней");
-  
-}
+
+int m = UserInputInt("Введите   длину строки масива ");
+int n = UserInputInt("Введите  длину  столбцов масива ");
+int[,] matrixResult = GetMatrix(m, n);
+PrintMatrix(matrixResult);
+Console.WriteLine();
+int indexLine = UserInputInt("Введите   индекс строки ");
+int indexСolumn = UserInputInt("Введите   индекс столба ");
+PrintIndexMatrix(matrixResult, indexLine, indexСolumn, m, n);
